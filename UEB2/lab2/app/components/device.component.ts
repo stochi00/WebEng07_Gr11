5a5c710c2036a225c1028ca3d9334d79a58f29a6
Stefan Kölbl <koelbl.s@gmx.at>
2017-04-22 12:48:48 +0200

    document click handler added

--- UEB2/lab2/app/components/device.component.ts
+++ UEB2/lab2/app/components/device.component.ts
@@ -1,10 +1,13 @@
-import {Component, Input, Output, EventEmitter } from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 import {Device} from '../model/device';
 import {Router} from '@angular/router';
 
 @Component({
     moduleId: module.id,
     selector: '[device-component]',
+    host: {
+        '(document:click)': 'handleClick($event)',
+    },
     templateUrl: '../views/device.html',
 })
 
@@ -13,34 +16,45 @@ export class DeviceComponent {
     @Output() deviceRemoved = new EventEmitter();
 
     public isEditable: boolean = false;
-    public temp_name:  string;
+    public temp_name: string;
 
-    constructor(private router: Router) { }
+    constructor(private router: Router) {
+    }
 
-    public removeItem()
-    {
-        if(!this.isEditable)
+    public removeItem() {
+        if (!this.isEditable)
             this.deviceRemoved.emit(this.device);
         this.toggleEdit(false);
     }
 
-    public editItem(value: string)
-    {
+    public editItem(value: string) {
         this.temp_name = value;
     }
 
-    public toggleEdit(save: boolean)
-    {
-        if(save)
+    public handleClick(event: any) {
+        var clickedComponent = event.target;
+
+        if (clickedComponent.nodeName == "INPUT") {
+            return;
+        }
+
+        if (clickedComponent.className == "device-edit") {
+            return;
+        }
+        
+        this.isEditable = false;
+    }
+
+    public toggleEdit(save: boolean) {
+        if (save)
             this.device.display_name = this.temp_name;
         else
             this.temp_name = this.device.display_name;
         this.isEditable = !this.isEditable;
     }
 
-    public clickedElement(){
-        if(!this.isEditable)
+    public clickedElement() {
+        if (!this.isEditable)
             this.router.navigate(['/details', this.device.id]);
     }
-
 }
--- UEB2/lab2/app/components/overview.component.ts
+++ UEB2/lab2/app/components/overview.component.ts
@@ -27,5 +27,4 @@ export class OverviewComponent{
         }
         this.devices.slice()
     }
-
 }
